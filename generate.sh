82702021ffe82cc1fb30d15701534b97aa0c3fc9
Nguyen Van Tri <vantri1010@gmail.com>
2024-11-22 01:57:12 +0700

    CRUD API with MongoDB

--- generate.sh
+++ generate.sh
@@ -3,4 +3,7 @@
 protoc --go_out=./ --go_opt=paths=source_relative greet/greetpb/greet.proto
 protoc --go-grpc_out=./ --go-grpc_opt=paths=source_relative greet/greetpb/greet.proto
 
+protoc --go_out=./ --go_opt=paths=source_relative blog/blogpb/blog.proto
+protoc --go-grpc_out=./ --go-grpc_opt=paths=source_relative blog/blogpb/blog.proto
+
 
